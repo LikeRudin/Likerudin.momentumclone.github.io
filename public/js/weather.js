ea7736578d2eb0390ff0fa341d1c64639a1c47b8
Young Jin <118636461+LikeRudin@users.noreply.github.com>
2023-08-22 16:45:40 +0900

    Update weather.js

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,70 +1,91 @@
-
-const weather = document.querySelector(`.weather__weather`);
-const city = document.querySelector(`.weather__city`);
+const weatherElement = document.querySelector(`.weather__weather`);
+const cityElement = document.querySelector(`.weather__city`);
 
 
 const API_KEY = "7a38e6a93c3e57a925e618636e9ba5fb";
 
-/**
- * show weather and city
- * @param {*} lan: langitude 
- * @param {*} lon: longtitude
- */
-const getWeather = function (lan, lon) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lan}&lon=${lon}&appid=${API_KEY}&units=metric`;
-     
-     fetch(apiUrl)
-     .then((response)=> response.json())
-     .then((data)=>{
-        console.log(data);
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-    });
-
-
+const fetchDayWeather =  async (lan, lon) => {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lan}&lon=${lon}&appid=${API_KEY}&units=metric`;    
+    const response = await fetch(apiUrl)
+    const data = response.json();
+    return data;    
 }
 
-/**
- * get current position.
- * @param {} position 
- */
-const onGeoOk = function (position) {
+const paintWeather = (data) => {
     const {
-            coords: {latitude, longitude}
-        } = position;
-
-    
-    sessionStorage.setItem("coords", JSON.stringify({latitude, longitude}));
-    
-    getWeather(latitude, longitude);
-   
+        name,
+        weather:{0: {main: weather}} ,
+        main: { temp },
+        } = data;
+    cityElement.innerText = name;
+    weatherElement.innerText = `${weather} / ${temp}`;
 }
 
-/**show Error message*/
-const onGeoError = function() {
-    alert(`Can't find you. No weather for you`);
-    city.innerText = "Nowhere";
-    weather.innerText = "raining quokkas and kangaroos"
+const paintError = () => {
+    cityElement.innerText = "no where";
+    weatherElement.innerText = "nothing";
 }
 
-/** get user's location information from sessionStorage if exists,
- * if doesn't, get that from navigator Object
- * @returns 
- */
-const loadWeather = function() {
+const fetchFiveDayWeather = async (lat,lon)=> {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data
+}
+
+const printFiveDayForecast = (data) =>{
+    const weatherList = data["list"]; 
+    const fiveWeathers = [];
+    for (let i = 7; 40 > i ; i += 8) {
+        const {dt_txt, weather} = weatherList[i]; 
+        fiveWeathers.push([dt_txt, weather[0]["main"]]);  
+    }
+    console.log(fiveWeathers);
+}
+
+const onGeoOk = (position) => {
+    const {
+        coords: {latitude, longitude}
+    } = position;
+    sessionStorage.setItem("coords", JSON.stringify({latitude, longitude}));   
+}
+
+const onGeoError = () => {
+    console.log("위치정보를 찾을 수 없습니다.");
+}
+
+
+const loadCoords = async () => {
     const coords = sessionStorage.getItem("coords");
+
     if (coords !== null) {
         const { latitude, longitude } = JSON.parse(coords);
-        getWeather(latitude, longitude);
-        return;
+        return {latitude, longitude}
     } else {
-        navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-    }
-}
+        const position = await new Promise((onGeoOk, onGeoError) =>{
+            navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+        });
+        const {latitude, longitude} = position.coords
+        return {latitude, longitude}
+}}
+
 
 /*initialize */
-const init = function () {
-    loadWeather();
+const init =  async () => {
+    try{
+        const {latitude, longitude} = await loadCoords();
+        
+        const weatherData = await fetchDayWeather(latitude, longitude);
+        paintWeather(weatherData);
+        
+        const forecastData = await fetchFiveDayWeather(latitude, longitude);
+        printFiveDayForecast(forecastData);
+
+    } catch (error){
+        console.log(error);
+        alert("failed to load");
+        paintError();
+    }
 }
 
 init();
